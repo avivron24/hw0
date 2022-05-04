5e115814adfcbcb658ffb0c66c40497c8fcc4f11
brarbrb <81331578+brarbrb@users.noreply.github.com>
2022-05-04 13:25:53 +0300

    Update Die.java

--- Die.java
+++ Die.java
@@ -2,21 +2,24 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Die {
+    private static final int DEFAULT_MIN = 1;
+    private static final int DEFAULT_MAX = 6;
+
 
     Random random = Main.rnd;
     int[] die;
-    int down;
-    int up;
+    int max;
+    int min;
 
 
-    public Die (int down , int up){
-        this.down = down;
-        this.up = up;
+    public Die (int max , int min){
+        this.max = max;
+        this.min = min;
     }
 
     public Die (){
-        int down = 1;
-        int up = 6;
+        this.min = DEFAULT_MIN;
+        this.max = DEFAULT_MAX;
     }
 
     public int roll (int down , int up ){
